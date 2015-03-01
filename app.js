a7a92fc31421834945fff65f71850c1963f644bc
Oskari Virtanen <oskari@flowdock.com>
2015-03-01 17:11:03 +0200

    Store events to db

--- app.js
+++ app.js
@@ -13,13 +13,30 @@ app.use(function(req, res, next) {
 
 var db = require("./models");
 
-var PORT = process.env.PORT || 3000;
+function createEvent(params) {
+  return db.event.create({
+    device_name: params.device.name,
+    device_id: params.device.id,
+    duration: params.duration,
+    connection: params.connection,
+    file: params.file,
+    host: params.host,
+    size: params.size,
+    meta: params.meta,
+    signal: params.signal
+  });
+}
 
 app.post("/event", function(req, res) {
-  db.sequelize.query("SELECT * FROM events;");
-  res.send("Hello world");
+  createEvent(req.body).then(function(event) {
+    res.status(201).json(event);
+  }, function(error) {
+    winston.error("Unexpected error when saving event to database: %s", error);
+    res.status(500).json({message: "Unknown error"});
+  });
 });
 
+var PORT = process.env.PORT || 3000;
 var server = app.listen(PORT, function() {
   var addressInfo = server.address();
   winston.info("Started server: %s:%s", addressInfo.address, addressInfo.port);
